7d9b2e64ed131e00675ac7d967791bf6988ec489
Andrii Popov <andrew.p@codesushi.co>
2018-02-09 18:52:40 +0000

    lesson 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // notice, warning, fatal error, parse error
+        // int, float, string, boolean + type casting
+        // !, && (*), || (+)
+        // <, >, >= , <=, ==, !=, ===, !== 
+        
+        $a = 123;
+        
+        echo $a;
+        var_dump($a);
+    ?>
+</body>
+</html>
