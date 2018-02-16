eca4e4aff7b96735d4af70a1d0ff5a7cb7b937b5
Andrii Popov <andrew.p@codesushi.co>
2018-02-16 17:47:25 +0000

    base list

--- index.php
+++ index.php
@@ -1,94 +1,115 @@
-<!DOCTYPE html>
+<?php
+    $employees = [];
+
+    $e1 = [
+        'first_name' => 'Andy',
+        'last_name' => 'Anderson',
+        'email' => 'andy@gmail.com'
+    ];
+    
+    $employees[] = $e1;
+    
+    $e2 = [
+        'first_name' => 'Andy2',
+        'last_name' => 'Anderson2',
+        'email' => 'andy@gmail.com2'
+    ];
+    
+    $employees[] = $e2;
+    
+    // var_dump($employees);
+
+?>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-        
-        // $i = 0;
-        
-        // do {
-        //     $a = rand(1, 100);
-        //     echo "{$a} <br>";
-        //     echo ++$i;
-        //     echo '<br> ----- <br>';
-        // } while ($i < 10);
-        
-        
-        // for ($i = 0; $i < 1000; $i++) {
-        //     $a = rand(1, 5);
-            
-        //     if ($a == 3) {
-        //         exit('B');
-        //     }
-            
-        //     echo "{$a} <br>";
-        //     echo $i;
-        //     echo '<br> ----- <br>';
-        // }
-        
-        
-        
-        // boolean, int, float, string, null - scalar
-        // array
-        
-        // implode explode
-        
-        // $a = [35, false, 123, [-1.2, true, 'yay']]; 
-        // $length = count($a);
-        // for ($i = 0; $i < $length; $i++) {
-        //     echo var_dump($a[$i]);
-        //     if (is_array($a[$i])) {
-        //         for ($j = 0; $j < count($a[$i]); $j++) {
-        //             var_dump($a[$i][$j]);
-        //         }
-        //     }
-        // }
-        
-        
-        // $student = [
-        //     'name' => 'Andy',
-        //     'age' => 25,
-        //     'city' => 'SF'
-        // ];
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        // var_dump($student);
+    <title>Starter Template for Bootstrap</title>
 
-        // foreach ($student as &$value) {
-        //     $value = rand(1,20);
-        //     // echo "{$value} ";
-        // }
-        // // unset($value);
-        
-        // $value = 'Hello';
-        
-        // var_dump($student);
-        
-        
-        echo 'one <br>' . print 'two <br>';
-        
-        
-        // $a = & $b;
-        
-        // $b = 1;
-        
-        // var_dump($a, $b);
-        
-        // $a = 350;
-        
-        // var_dump($a, $b);
-        
-        // $b = 100;
-        
-        // var_dump($a, $b);
-        
-        
-        
-        
-        
-        
-      
-    ?>
-</body>
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+
+    <div class="starter-template">
+        <h1>Employees</h1>
+    </div>
+    
+    <?php if (rand(-1, 1)) : ?>
+        <h1>Hello</h1>
+    <?php endif ?>
+
+    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+            
+            <?php foreach ($employees as $employee) : ?>
+                <tr>
+                    <td><?=$employee['first_name'] ?></td>
+                    <td><?=$employee['last_name'] ?></td>
+                    <td><?=$employee['email'] ?></td>
+                </tr>  
+            <?php endforeach ?>
+            
+        </tbody>
+    </table>
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
 </html>
---
+++ test.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        
+        // $i = 0;
+        
+        // do {
+        //     $a = rand(1, 100);
+        //     echo "{$a} <br>";
+        //     echo ++$i;
+        //     echo '<br> ----- <br>';
+        // } while ($i < 10);
+        
+        
+        // for ($i = 0; $i < 1000; $i++) {
+        //     $a = rand(1, 5);
+            
+        //     if ($a == 3) {
+        //         exit('B');
+        //     }
+            
+        //     echo "{$a} <br>";
+        //     echo $i;
+        //     echo '<br> ----- <br>';
+        // }
+        
+        
+        
+        // boolean, int, float, string, null - scalar
+        // array
+        
+        // implode explode
+        
+        // $a = [35, false, 123, [-1.2, true, 'yay']]; 
+        // $length = count($a);
+        // for ($i = 0; $i < $length; $i++) {
+        //     echo var_dump($a[$i]);
+        //     if (is_array($a[$i])) {
+        //         for ($j = 0; $j < count($a[$i]); $j++) {
+        //             var_dump($a[$i][$j]);
+        //         }
+        //     }
+        // }
+        
+        
+        // $student = [
+        //     'name' => 'Andy',
+        //     'age' => 25,
+        //     'city' => 'SF'
+        // ];
+
+        // var_dump($student);
+
+        // foreach ($student as &$value) {
+        //     $value = rand(1,20);
+        //     // echo "{$value} ";
+        // }
+        // // unset($value);
+        
+        // $value = 'Hello';
+        
+        // var_dump($student);
+        
+        
+        echo 'one <br>' . print 'two <br>';
+        
+        
+        // $a = & $b;
+        
+        // $b = 1;
+        
+        // var_dump($a, $b);
+        
+        // $a = 350;
+        
+        // var_dump($a, $b);
+        
+        // $b = 100;
+        
+        // var_dump($a, $b);
+        
+        
+        
+        
+        
+        
+      
+    ?>
+</body>
+</html>
