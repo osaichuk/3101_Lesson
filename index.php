7b917b1fd9a81c42e1b1f6b5df4d10e20028f055
Andrii Popov <andrew.p@codesushi.co>
2018-02-16 18:46:54 +0000

    test changes

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
     </div>
     
     <?php if (rand(-1, 1)) : ?>
-        <h1>Hello</h1>
+        <h1>Hello world</h1>
     <?php endif ?>
 
     <table class="table table-hover">
@@ -104,7 +104,7 @@
           <tr>
             <th>Firstname</th>
             <th>Lastname</th>
-            <th>Email</th>
+            <th>Email address</th>
           </tr>
         </thead>
         <tbody>
